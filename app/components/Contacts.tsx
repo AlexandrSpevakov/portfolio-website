53d2eb4c6f2c1ebdf8ed1d003e73ae6661c9610e
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-28 20:03:07 -0400

    feat: implement angle on the Contact section

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -1,16 +1,12 @@
 export default function Contacts() {
   return (
-    <>
-      <section className="relative isolate text-white after:absolute after:inset-0 after:z-[-1] after:skew-y-[6deg] after:bg-black">
-        <section
-          id="about"
-          className="relative mx-auto mt-32 w-10/12 bg-cover bg-center bg-no-repeat py-28"
-        >
-          <h2 className="text-center font-archivo text-7.5xl">Contacts</h2>
-        </section>
-        {/* <section className="bg-black h-40" /> */}
+    <section className="relative isolate text-white before:absolute before:inset-0 before:z-[-1] before:skew-y-[-6deg] before:bg-black after:absolute after:left-0 after:bottom-[-140px] after:z-[-1] after:h-full after:w-full after:bg-black">
+      <section
+        id="about"
+        className="relative mx-auto mt-32 w-10/12 bg-cover bg-center bg-no-repeat py-28"
+      >
+        <h2 className="text-center font-archivo text-7.5xl">Contacts</h2>
       </section>
-      {/* <section className="bg-black h-40" /> */}
-    </>
+    </section>
   );
 }
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <section className=" bg-black text-white">
       <footer
         id="about"
-        className="relative mx-auto mt-96 w-10/12 bg-cover bg-center bg-no-repeat"
+        className="relative mx-auto w-10/12 bg-cover bg-center bg-no-repeat"
       >
         <h2 className="text-center font-archivo text-7.5xl">Footer</h2>
       </footer>
