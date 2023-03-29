2a02aab01f1083682788332611b6119334036a0d
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-03-29 13:58:10 -0400

    feat: implement Footer on 4k size

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <section className="mt-[139px] bg-black pb-6 text-white">
+    <section className="mt-[139px] bg-black pb-6 text-white 2xl:pb-8 3xl:pb-10">
       <footer className="relative mx-auto w-10/12 bg-cover bg-center bg-no-repeat">
-        <span className="block text-center font-archivo">
+        <span className="block text-center font-archivo 2xl:text-lg 3xl:text-xl">
           &copy; Oleksandr Spievakov 2023
         </span>
       </footer>
