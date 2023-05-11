e3433ee9bf73b726c12d9c4faa2d2942893528ea
Oleksandr Spievakov <alexandr.pobeda99@gmail.com>
2023-05-11 06:00:41 -0400

    feat: add photo of the first project

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import logo from '../../public/logo.png';
+// import logo from '../../public/logo.png';
 
 export default function Logo() {
   return (
@@ -9,11 +9,11 @@ export default function Logo() {
       href="/"
       className="flex w-fit items-center rounded outline-none focus:outline-offset-4 focus:outline-white"
     >
-      <Image
-        src={logo}
+      {/* <Image
+        // src={logo}
         alt="Logo"
         className="w-48 sm:w-52 md:w-56 lg:w-64 2xl:w-72 3xl:w-80"
-      />
+      /> */}
     </Link>
   );
 }
