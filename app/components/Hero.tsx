cfc3f5d4ca4fe524cb9d0a43985d8842fbae751b
Sasha Spievakov <89634319+SashaSpievakov@users.noreply.github.com>
2023-05-26 23:11:39 -0400

    refactor: change the title

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section className="mx-auto h-40 max-w-screen-xl px-4 sm:h-56 sm:px-6 md:h-72 md:px-10 xl:h-96">
       <h1 className="mt-16 font-montserrat text-2xl font-semibold min-[360px]:text-3xl sm:mt-24 sm:text-4xl md:mt-32 md:text-6xl md:leading-tight xl:text-7xl xl:leading-extra-tight">
-        Sasha Spievakov <br /> — Software Engineer
+        Sasha Spievakov <br /> — Frontend Developer
       </h1>
     </section>
   );
